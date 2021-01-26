bdd9e74ba59019296943d438cd6d5f89a9931956
pilarguz10 <pilar1494@hotmail.com>
2021-01-26 19:01:34 +0000

    A falta de botonDeleteAll

--- src/js/component/TaskList.jsx
+++ src/js/component/TaskList.jsx
@@ -1,49 +1,147 @@
 import React, { useState, useEffect } from "react";
 import { Button } from "bootstrap";
 
-export function TaskList(e) {
-	const [task, setTask] = useState([]);
+export const TaskList = () => {
+	const [taskApi, setTaskApi] = useState([]);
 	const [text, setText] = useState("");
+	const [listElement, setListElement] = useState([]);
 
 	const styleObj = {
 		color: "black"
 	};
 
-	let TextValue = e => {
-		setText(e.target.value);
+	// let TextValue = e => {
+	// 	setText(e.target.value);
+	// };
+
+	// let TaskAdd = event => {
+	// 	if (event.key === "Enter") {
+	// 		let newTask = event.target.value;
+	// 		if (newTask) {
+	// 			setTaskApi(taskApi => [...taskApi, text]);
+	// 			let lista = document.querySelector("#inforeceive");
+	// 			lista.value = "";
+	// 		}
+	// 	}
+	// };
+
+	//Eliminar task
+	let removeItem = id => {
+		setTaskApi(taskApi => {
+			return taskApi.filter((item, index) => {
+				return index !== id;
+			});
+		});
 	};
-	let TaskAdd = event => {
-		if (event.key === "Enter") {
-			let newTask = event.target.value;
-			if (newTask) {
-				setTask(task => [...task, text]);
-				let lista = document.querySelector("#inforeceive");
-				lista.value = "";
-			}
-		}
-	};
 
-	let TaskDelete = i => {
-		task.splice(i, 1);
-	};
-
-	const [listItem, setListItem] = useState(null);
-
-	useEffect(() => {
-		setListItem(
-			task.map((taskitem, i) => {
-				return (
-					<li key={i} className="myLi">
-						{taskitem}
-
-						<button className="btn" onClick={() => TaskDelete(i)}>
-							<i className="fas fa-times" />
-						</button>
-					</li>
-				);
+	//Función para marcar como "realizada una tarea"
+	const isDone = indexToCross => {
+		setTaskApi(
+			taskApi.map((task, index) => {
+				if (index == indexToCross) {
+					task.done = !task.done;
+				}
+				return task;
 			})
 		);
-	});
+	};
+
+	let url = "https://assets.breatheco.de/apis/fake/todos/user/pilarguz10";
+	let optionsPut = {
+		method: "PUT",
+		body: JSON.stringify(taskApi),
+		headers: {
+			"Content-Type": "application/json"
+		}
+	};
+	let optionsGET = {
+		method: "GET"
+	};
+
+	fetch(url, optionsPut)
+		.then(respuesta => {
+			if (respuesta.status >= 200 && respuesta.status < 300) {
+				console.log("Método PUT OK");
+				return respuesta.json();
+			}
+		})
+
+		.then(body => {
+			console.log("Body:", JSON.stringify(body));
+		})
+
+		.catch(error => console.error("Error:" + error));
+
+	useEffect(() => {
+		//Fetch con método GET para obtener las tareas de la API
+		fetch(url, optionsGET)
+			.then(respuesta => {
+				if (respuesta.status >= 200 && respuesta.status < 300) {
+					console.log("El request se hizo bien");
+					return respuesta.json();
+				}
+			})
+
+			.then(responseAsJson => {
+				setTaskApi(responseAsJson);
+			})
+
+			.catch(error => console.error("Error:" + error));
+	}, []); //Fin de useEffect
+
+	//Cuando se actualice taskApi
+	useEffect(
+		() => {
+			//Fetch PUT para actualizar la lista en la API
+			fetch(url, optionsPut)
+				.then(respuesta => {
+					if (respuesta.status >= 200 && respuesta.status < 300) {
+						console.log("Método PUT OK");
+						return respuesta.json();
+					}
+				})
+
+				.then(body => {
+					console.log("Body:", JSON.stringify(body));
+				})
+
+				.catch(error => console.error("Error:" + error));
+
+			setListElement(
+				taskApi.map((task, index) => {
+					let crossTask = "myLi";
+					if (task.done) {
+						crossTask += " taskDone";
+					}
+					return (
+						//<div className={crossTask}
+
+						<li key={index} className={crossTask}>
+							{task.label}
+							<button
+								className="btnRemove"
+								onClick={() => removeItem(index)}>
+								<i className="fas fa-times" />
+							</button>
+
+							<button
+								id="done"
+								className="btnVisible"
+								onClick={() => {
+									isDone(index);
+								}}>
+								<i className="fas fa-check" />
+							</button>
+						</li>
+
+						//</div>
+					);
+				})
+			);
+		},
+		[taskApi]
+	);
+
 	return (
 		<div className="container">
 			<div className="card container ">
@@ -51,25 +149,38 @@ export function TaskList(e) {
 					<i className="fas fa-thumbtack fa-lg" />
 				</div>
 				<h1 style={styleObj}>TO DO LIST</h1>
-				<input
-					id="inforeceive"
-					className="myInput"
-					type="text"
-					placeholder="Add task"
-					onChange={e => TextValue(e)}
-					onKeyPress={event => {
-						TaskAdd(event);
-					}}
-				/>
+				<form
+					onSubmit={event => {
+						event.preventDefault();
+						if (text != "") {
+							//doesn't allow adding tasks without content
+							setTaskApi([
+								...taskApi,
+								{ label: text, done: false }
+							]);
+							setText("");
+						}
+					}}>
+					<input
+						type="text"
+						placeholder="New task"
+						value={text}
+						onChange={event => setText(event.target.value)}
+					/>
+				</form>
+
 				<ul id="myUlist" className="myUl">
-					{listItem}
+					{listElement}
 				</ul>
 				<div>
 					<span className="badge badge-light">
-						{task.length + ""} Tareas
+						{taskApi.length} Tareas
 					</span>
+					<button class="btn">
+						<i class="fa fa-trash" />
+					</button>
 				</div>
 			</div>
 		</div>
 	);
-}
+}; //Fin de la función principal
